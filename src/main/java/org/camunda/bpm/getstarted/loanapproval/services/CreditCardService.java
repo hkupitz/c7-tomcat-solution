3cda678eb219a8c253e0add7b15e77f7b94efccf
Hendrik Kupitz <hendrik.kupitz@camunda.com>
2024-09-18 10:17:50 +0200

    Fix solution

--- src/main/java/org/camunda/bpm/getstarted/loanapproval/services/CreditCardService.java
+++ src/main/java/org/camunda/bpm/getstarted/loanapproval/services/CreditCardService.java
@@ -5,11 +5,6 @@ public class CreditCardService {
 	public void chargeAmount(String cardNumber, String cvc, String expiryDate, Double amount) {
 		System.out.printf("charging card %s that expires on %s and has cvc %s with amount of %f %s", cardNumber, expiryDate, cvc, amount, System.lineSeparator());
 		System.out.println("payment completed");
-
-		if (validateExpiryDate(expiryDate)) {
-			throw new 
-		}
-
 	}
 
 
